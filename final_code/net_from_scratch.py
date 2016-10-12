cd1bec59151fa1508511026cf86ef890d48cf2f4
gabrielarpino <arpino.gabriel@gmail.com>
2016-10-12 19:28:29 -0400

    added axes to error graph

--- final_code/net_from_scratch.py
+++ final_code/net_from_scratch.py
@@ -3,14 +3,18 @@ from autograd import elementwise_grad
 from autograd import grad
 import matplotlib.pyplot as plt
 
+#Define the objective function (sigmoid)
 def objective(a):
 	return 1/(1 + np.exp(a))
 
+#Define the input arrays x and the desired output array y
 x = np.array([[1,1,0]
 		,[1,0,0]
 		,[1,1,1]
 		,[0,1,1]])
 y = np.array([[0,0,1,1]]).T
+
+#Declare the desired learning rate for the network
 learning_rate = 1
 
 np.random.seed(1)
@@ -24,10 +28,12 @@ fig = plt.figure(figsize=(12, 8))
 ax = fig.add_subplot(111, frameon=False)
 plt.ion()
 plt.show()
+#ax.title('Output Error', color='#afeeee')
 count = 0
 
 for iter in xrange(60000):
 
+	#Forward Propagation
 	l0 = x
 	l1 = objective(np.dot(l0,syn0))
 	l2 = objective(np.dot(l1,syn1))
@@ -35,23 +41,27 @@ for iter in xrange(60000):
 
 	objective_deriv = elementwise_grad(objective)
 
+	#Backward Propagation
 	delta_output = y - l3
 
 	delta_l3 = delta_output*objective_deriv(l3)
-	l2_error = np.dot(delta_l3, syn2.T)	#produces a 4x4 matrix
+	l2_error = np.dot(delta_l3, syn2.T)	
 
 	delta_l2 = l2_error*objective_deriv(l2)
 	l1_error = np.dot(delta_l2, syn1.T)
 
 	delta_l1 = l1_error*objective_deriv(l1)
 
+	#Adjust the neural synapses according to the errors produced
 	syn2 += learning_rate*l2.T.dot(delta_l3)
 	syn1 += learning_rate*l1.T.dot(delta_l2)
 	syn0 += learning_rate*l0.T.dot(delta_l1)
 
-	#plot
+	#Plot the error as it changes during the back-propagation steps
 	count = count + 1
 	ax.plot(delta_output)
+	plt.ylabel('Error')
+	plt.xlabel('Output Index')
 	plt.draw()
 	plt.pause(1.0/100000000.0)
 
